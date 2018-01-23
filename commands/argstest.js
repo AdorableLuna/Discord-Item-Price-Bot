fc77108936c43d20f83663703e1e7401b27d4428
Brian van Rheenen <brian@moodles.nl>
2018-01-23 11:43:57 +0100

    Removed dev files.

--- commands/argstest.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-    let arg = args.join(" ");
-    console.log("Lorem Ipsum " + arg + " Dolor Sit");
-}
-
-module.exports.help = {
-    name: "argstest"
-}
